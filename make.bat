d87c943e043fc0825b524b0941caf78d4ac6b248
qiuxingmao <geyuanliu@gmail.com>
2023-03-10 10:59:37 +0800

    2023-3-10

---
+++ make.bat
@@ -0,0 +1,6 @@
+python jemdoc.py group.jemdoc
+python jemdoc.py index-cn.jemdoc
+python jemdoc.py index.jemdoc
+python jemdoc.py publications.jemdoc
+python jemdoc.py service.jemdoc
+python jemdoc.py teaching.jemdoc
