549f6e072e86a86e0c667839a638edd24c156392
geyuanliu <geyualiu@gmail.com>
2024-10-28 09:07:28 +0800

    update 2024-10-28

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @REM python2 jemdoc.py group.jemdoc
 @REM python2 jemdoc.py index-cn.jemdoc
-python2 jemdoc.py index.jemdoc
-@REM python2 jemdoc.py publications.jemdoc
+@REM python2 jemdoc.py index.jemdoc
+python2 jemdoc.py publications.jemdoc
 @REM python2 jemdoc.py service.jemdoc
 @REM python2 jemdoc.py teaching.jemdoc
